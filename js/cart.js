d096774db00612774f89d8520b11bec218d59597
yohanalonso <yaaf.2009@gmail.com>
2020-10-01 09:05:33 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,18 @@
 var products = {};
 var subtotal = 0;
 var envio = 0;
+var cant = [];
+
+function changeCant() {
+    for (let i = 0; i < cant.length; i++) {
+        cant[i] = document.getElementById("cant" + i).value;
+    }
+}
 
 function showCart(array) {
     let htmlContentToAppend = "";
     for (let i = 0; i < array.length; i++) {
         let product = array[i];
-
         htmlContentToAppend += `
             <div class="row">
                     <div class="col-md-2">
@@ -14,15 +20,20 @@ function showCart(array) {
                     </div>
                     <p class="col-md-4">` + product.name + `</p>
                     <p class="col-md-2">` + product.currency + ` ` + product.unitCost + `</p>
-                    <div class="col-md-2"><input class="form-control" type="number" placeholder="` + product.count + `" id="count" value="` + product.count + `"></div>
-                    <p class="col-md-2" id="subtotal">` + product.currency + ` ` + product.unitCost * product.count + `</p>
+                    <div class="col-md-2"><input class="form-control" type="number" placeholder="` + product.count + `" id="cant` + i + `" value="` + cant[i] + `"></div>
+                    <p class="col-md-2">` + product.currency + ` ` + product.unitCost * cant[i] + `</p>
             </div>
             <hr>
             `
-        subtotal += product.unitCost * product.count;
+        subtotal += product.unitCost * cant[i];
     }
 
     document.getElementById("cart").innerHTML = htmlContentToAppend;
+    document.getElementById("productCostText").innerHTML = `UYU ` + subtotal;
+    document.getElementById("comissionText").innerHTML = `UYU ` + envio;
+    document.getElementById("totalCostText").innerHTML = `UYU ` + (envio + subtotal);
+    subtotal = 0;
+    envio = 0;
 }
 
 
@@ -33,10 +44,17 @@ document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(CART_INFO_URL).then(function(resultObj) {
         if (resultObj.status === "ok") {
             products = resultObj.data;
+            for (let i = 0; i < products.articles.length; i++) {
+                let product = products.articles[i];
+                cant.push(product.count);
+            }
             showCart(products.articles);
-            document.getElementById("productCostText").innerHTML = `UYU ` + subtotal;
-            document.getElementById("comissionText").innerHTML = `UYU ` + envio;
-            document.getElementById("totalCostText").innerHTML = `UYU ` + (envio + subtotal);
         }
     });
+
+    document.getElementById("cart").addEventListener("change", function() {
+        changeCant();
+        showCart(products.articles);
+    });
+
 });
