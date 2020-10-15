3435903a4e005db2e973bf58967f3a47e476c57e
yohanalonso <yaaf.2009@gmail.com>
2020-10-15 11:01:21 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,25 @@
 var products = {};
 var subtotal = 0;
 var envio = 0;
+var price = [];
 const dolar = 40;
-var moneda = "UYU "
+var moneda = "UYU ";
+var formaDePago = "";
 
 function changeCant(i) {
     document.getElementById("price" + i).innerHTML = products.articles[i].currency + ` ` + products.articles[i].unitCost * document.getElementById("cant" + i).value;
-    for (let j = 0; j < products.articles.length; j++) {
-        if ((moneda == "USD ") && (products.articles[j].currency == "UYU")) {
-            subtotal += products.articles[j].unitCost * document.getElementById("cant" + j).value / dolar;
-        } else {
-            subtotal += products.articles[j].unitCost * document.getElementById("cant" + j).value;
-        }
+    if ((moneda == "USD ") && (products.articles[i].currency == "UYU")) {
+        subtotal -= price[i];
+        price[i] = products.articles[i].unitCost * document.getElementById("cant" + i).value / dolar;
+        subtotal += products.articles[i].unitCost * document.getElementById("cant" + i).value / dolar;
+    } else {
+        subtotal -= price[i];
+        subtotal += products.articles[i].unitCost * document.getElementById("cant" + i).value;
+        price[i] = products.articles[i].unitCost * document.getElementById("cant" + i).value;
     }
     document.getElementById("productCostText").innerHTML = moneda + subtotal;
-    document.getElementById("comissionText").innerHTML = moneda + envio;
-    document.getElementById("totalCostText").innerHTML = moneda + (envio + subtotal);
-    subtotal = 0;
+    document.getElementById("comissionText").innerHTML = moneda + (envio * subtotal).toFixed(2);
+    document.getElementById("totalCostText").innerHTML = moneda + (envio * subtotal + subtotal).toFixed(2);
 }
 
 function showCart(array) {
@@ -31,23 +34,24 @@ function showCart(array) {
                     <p class="col-md-4">` + product.name + `</p>
                     <p class="col-md-2">` + product.currency + ` ` + product.unitCost + `</p>
                     <div class="col-md-2"><input onchange= changeCant(` + i + `) class="form-control" type="number" min="0" placeholder="` + product.count + `" id="cant` + i + `" value="` + product.count + `"></div>
-                    <p class="col-md-2" id="price` + i + `">` + product.currency + ` ` + product.unitCost * product.count + `</p>
+                    <p class="col-md-2" id="price` + i + `">` + product.currency + ` ` + price[i] + `</p>
             </div>
             <hr>
             `
         if ((moneda == "USD ") && (product.currency == "UYU")) {
-            subtotal += product.unitCost * product.count / dolar;
+            price[i] = product.unitCost * product.count / dolar;
+            subtotal += price[i];
         } else {
-            subtotal += product.unitCost * product.count;
+            subtotal += price[i];
         }
     }
 
     document.getElementById("cart").innerHTML = htmlContentToAppend;
     document.getElementById("productCostText").innerHTML = moneda + subtotal;
-    document.getElementById("comissionText").innerHTML = moneda + envio;
-    document.getElementById("totalCostText").innerHTML = moneda + (envio + subtotal);
-    subtotal = 0;
+    document.getElementById("comissionText").innerHTML = moneda + (envio * subtotal).toFixed(2);
+    document.getElementById("totalCostText").innerHTML = moneda + (envio * subtotal + subtotal).toFixed(2);
 }
+
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -58,15 +62,67 @@ document.addEventListener("DOMContentLoaded", function(e) {
         if (resultObj.status === "ok") {
             products = resultObj.data;
             let j = 0;
-            while (products.articles[j].currency == "UYU") {
-                j++;
-            }
-            if (products.articles[j].currency != "UYU") {
-                moneda = "USD "
+            for (let i = 0; i < products.articles.length; i++) {
+                price.push(products.articles[i].unitCost * products.articles[i].count);
+                if (products.articles[i].currency == "USD") {
+                    moneda = "USD ";
+                }
             }
             showCart(products.articles);
         }
     });
-
-
+    document.getElementById("tarjet").addEventListener("click", function() {
+        formaDePago = `
+        <label for="name" class="">Nombre y Apellido:</label>
+        <input type="text" id="name" class="form-control col-md-6" placeholder="Nombre y Apellido">
+        <label for="number" class="">Número de tarjeta:</label>
+        <input type="text" id="number" class="form-control col-md-4" placeholder="Número de tarjeta">
+        <label for="securityNumber" class="">Número de seguridad:</label>
+        <input type="text" id="securityNumber" class="form-control col-md-2" placeholder="Nro de seguridad">
+        <label for="date" class="">Fecha de vencimiento:</label>
+        <input type="text" id="date" class="form-control col-md-3" placeholder="Fecha de vencimiento">
+        `
+    });
+    document.getElementById("transf").addEventListener("click", function() {
+        formaDePago = `
+        <label for="name" class="">Nombre y Apellido:</label>
+        <input type="text" id="name" class="form-control col-md-6" placeholder="Nombre y Apellido">
+        <label for="number" class="">Número de cuenta:</label>
+        <input type="text" id="number" class="form-control col-md-4" placeholder="Número de cuenta">
+        <label for="bank" class="">Banco:</label>
+        <input type="text" id="bank" class="form-control col-md-2" placeholder="Banco">
+        <label for="sucursal" class="">Sucursal</label>
+        <input type="text" id="sucursal" class="form-control col-md-3" placeholder="Sucursal">
+        `
+    });
+    document.getElementById("cash").addEventListener("click", function() {
+        formaDePago = `
+        <label for="name" class="">Nombre y Apellido:</label>
+        <input type="text" id="name" class="form-control col-md-6" placeholder="Nombre y Apellido">
+        <label for="red" class="">Red de cobranza:</label>
+        <select class="custom-select d-block col-md-2" id="red">
+            <option value="">Elija la red</option>
+                    <option>Abitab</option>
+                    <option>RedPago</option>
+            </select>
+        `
+    });
+    document.getElementById("acept").addEventListener("click", function() {
+        document.getElementById("waytopay").innerHTML = formaDePago;
+    });
+    document.getElementById("premiumradio").addEventListener("click", function() {
+        envio = 0.15;
+        document.getElementById("comissionText").innerHTML = moneda + (envio * subtotal).toFixed(2);
+        document.getElementById("totalCostText").innerHTML = moneda + (envio * subtotal + subtotal).toFixed(2);
+    });
+    document.getElementById("expressradio").addEventListener("click", function() {
+        envio = 0.07;
+        document.getElementById("comissionText").innerHTML = moneda + (envio * subtotal).toFixed(2);
+        document.getElementById("totalCostText").innerHTML = moneda + (envio * subtotal + subtotal).toFixed(2);
+    });
+    document.getElementById("standardradio").addEventListener("click", function() {
+        envio = 0.05;
+        document.getElementById("comissionText").innerHTML = moneda + (envio * subtotal).toFixed(2);
+        document.getElementById("totalCostText").innerHTML = moneda + (envio * subtotal + subtotal).toFixed(2);
+    });
 });
