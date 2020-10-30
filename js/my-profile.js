2536e5287bc822f977201f22edafa9d441d8cb77
yohanalonso <yaaf.2009@gmail.com>
2020-10-30 18:36:34 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,37 @@
+var dato = [];
+
+function showInfo() {
+    let info = localStorage.getItem('guardado');
+    if (info != undefined) {
+        let datos = JSON.parse(info);
+        document.getElementById("firstName").placeholder = datos[0].firstName;
+        document.getElementById("secondName").placeholder = datos[0].secondName;
+        document.getElementById("firstLastname").placeholder = datos[0].firstLastname;
+        document.getElementById("secondLastname").placeholder = datos[0].secondLastname;
+        document.getElementById("age").placeholder = datos[0].age;
+        document.getElementById("email").placeholder = datos[0].email;
+        document.getElementById("cel").placeholder = datos[0].cel;
+    }
+
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function(e) {
+    showInfo();
+    document.getElementById("save").addEventListener("click", function() {
+        let info = {
+            "firstName": document.getElementById("firstName").value,
+            "secondName": document.getElementById("secondName").value,
+            "firstLastname": document.getElementById("firstLastname").value,
+            "secondLastname": document.getElementById("secondLastname").value,
+            "age": document.getElementById("age").value,
+            "email": document.getElementById("email").value,
+            "cel": document.getElementById("cel").value
+        }
+        dato.push(info);
+        localStorage.setItem('guardado', JSON.stringify(dato));
+        showInfo();
+    });
 
 });
