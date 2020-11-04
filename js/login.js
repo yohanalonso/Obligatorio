c91b7fc20ad9df5322a21caacc364cd7c3224680
yohanalonso <yaaf.2009@gmail.com>
2020-11-04 16:07:36 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,28 @@
 function validar(valor) {
-    return !(valor == null || valor.length == 0);
+    if (!(valor.value == null || valor.value.length == 0)) {
+        valor.classList.remove('is-invalid');
+        valor.classList.remove('is-valid');
+        valor.classList.add('is-valid');
+    } else {
+        valor.classList.remove('is-invalid');
+        valor.classList.remove('is-valid');
+        valor.classList.add('is-invalid');
+    }
+    return !(valor.value == null || valor.value.length == 0);
 }
 
 function validarEmail(valor) {
     let email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-    return email.test(valor2);
+    if (email.test(valor.value)) {
+        valor.classList.remove('is-invalid');
+        valor.classList.remove('is-valid');
+        valor.classList.add('is-valid');
+    } else {
+        valor.classList.remove('is-invalid');
+        valor.classList.remove('is-valid');
+        valor.classList.add('is-invalid');
+    }
+    return email.test(valor.value);
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -13,8 +31,10 @@ function validarEmail(valor) {
 document.addEventListener("DOMContentLoaded", function(e) {
     document.getElementById("signin").addEventListener("submit", e => {
         e.preventDefault();
-        valor1 = document.getElementById("inputPassword").value;
-        valor2 = document.getElementById("inputEmail").value;
+        valor1 = document.getElementById("inputPassword");
+        valor2 = document.getElementById("inputEmail");
+        validarEmail(valor2);
+        validar(valor1);
         if (validar(valor1) && validarEmail(valor2)) {
             localStorage.setItem('usuario', valor2);
             window.location.href = "index1.html";
